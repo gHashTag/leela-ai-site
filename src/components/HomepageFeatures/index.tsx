e8aaa342233757759654d1aef540766d6e5f6399
Dmitriy Vasilev <raoffonom@icloud.com>
2023-06-22 15:50:51 +0700

    first commit

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import clsx from 'clsx'
+import styles from './styles.module.css'
+
+type FeatureItem = {
+  title: string
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>
+  description: JSX.Element
+}
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Leela AI is designed for maximum user experience. Installing and getting started with the application is easy
+        and intuitive.
+      </>
+    )
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Leela AI allows you to focus on what matters most - your self-discovery and spiritual growth. We take care of
+        all routine tasks so that you can immerse yourself in the development process as much as possible.
+      </>
+    )
+  },
+  {
+    title: 'Supported by AI',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Leela AI uses advanced AI technology to provide you with a unique and personalized experience. Using AI
+        technology, Leela can adapt to your needs and offer relevant and in-depth knowledge.
+      </>
+    )
+  }
+]
+
+function Feature({ title, Svg, description }: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import clsx from 'clsx'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
+import HomepageFeatures from '@site/src/components/HomepageFeatures'
+
+import styles from './index.module.css'
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext()
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link className="button button--secondary button--lg" to="/docs/intro">
+            Read documentation️
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext()
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Leela AI uses advanced AI technology to provide you with a unique and personalized experience. Using AI technology, Leela can adapt to your needs and offer relevant and in-depth knowledge."
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  )
+}
