e52d5c38770759523bde2b94eafd52b02ca22805
Dmitriy Vasilev <raoffonom@icloud.com>
2023-12-30 01:08:42 +0500

    footer update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,160 +1,166 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Leela Ai',
+  title: "Leela Ai",
   tagline:
     "Leela AI is an application that is a game of self-knowledge and spiritual development based on Vedic wisdom. Your guide here will be Leela's digital avatar, who will facilitate your path to a deep understanding of the nature of all things.",
-  favicon: 'img/favicon.ico',
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://leelachakra.com',
+  url: "https://leelachakra.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'gHashTag', // Usually your GitHub org/user name.
-  projectName: 'leela-ai-site', // Usually your repo name.
+  organizationName: "gHashTag", // Usually your GitHub org/user name.
+  projectName: "leela-ai-site", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'ru'],
+    defaultLocale: "en",
+    locales: ["en", "ru"],
     localeConfigs: {
       en: {
-        htmlLang: 'en-GB'
-      }
-    }
+        htmlLang: "en-GB",
+      },
+    },
   },
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/'
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/'
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css')
-        }
-      })
-    ]
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Leela Ai',
+        title: "Leela Ai",
         logo: {
-          alt: 'Leela Ai Logo',
-          src: 'img/logo.png'
+          alt: "Leela Ai Logo",
+          src: "img/logo.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs'
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
           },
 
           // { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://www.instagram.com/leela.chakra/',
-            label: 'Blog',
-            position: 'left'
+            href: "https://www.instagram.com/leela.chakra/",
+            label: "Blog",
+            position: "left",
           },
           {
-            type: 'localeDropdown',
-            position: 'right'
-          }
+            type: "localeDropdown",
+            position: "right",
+          },
           // {
           //   href: 'https://github.com/facebook/docusaurus',
           //   label: 'GitHub',
           //   position: 'right'
           // }
-        ]
+        ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro'
-              }
-            ]
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Instagram',
-                href: 'https://www.instagram.com/leela.chakra/'
+                label: "Instagram",
+                href: "https://www.instagram.com/leela.chakra/",
               },
               {
-                label: 'Facebook',
-                href: 'https://www.facebook.com/leelachakraapp'
+                label: "Facebook",
+                href: "https://www.facebook.com/leelachakraapp",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus'
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
               },
               {
-                label: 'Telegram',
-                href: 'https://t.me/leelachakraapp'
+                label: "Telegram RU",
+                href: "https://t.me/leelachakraapp",
               },
               {
-                label: 'VK',
-                href: 'https://vk.com/leela.chakra'
-              }
-            ]
+                label: "Telegram",
+                href: "https://t.me/leelachakraai",
+              },
+              {
+                label: "VK",
+                href: "https://vk.com/leela.chakra",
+              },
+            ],
           },
           {
-            title: 'Download App',
+            title: "Download App",
             items: [
               {
-                label: 'App Store',
-                href: 'https://apps.apple.com/ru/app/%D0%B8%D0%B3%D1%80%D0%B0-%D0%BB%D0%B8%D0%BB%D0%B0/id1296604457'
+                label: "App Store",
+                href: "https://apps.apple.com/ru/app/%D0%B8%D0%B3%D1%80%D0%B0-%D0%BB%D0%B8%D0%BB%D0%B0/id1296604457",
               },
               {
-                label: 'Google Play',
-                href: 'https://play.google.com/store/apps/details?id=com.leelagame&hl=ru'
-              }
-            ]
-          }
+                label: "Google Play",
+                href: "https://play.google.com/store/apps/details?id=com.leelagame&hl=ru",
+              },
+            ],
+          },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Leela Ai, Inc. Built with Docusaurus.`
+        copyright: `Copyright © ${new Date().getFullYear()} Leela Ai, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
-      }
-    })
-}
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
 
-module.exports = config
+module.exports = config;
